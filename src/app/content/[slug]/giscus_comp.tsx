f783c43dd6fa0c81ab6d89dfb487ef1686fe0a17
MuniMunii <tyuimiti@gmail.com>
2025-06-20 01:22:56 +0700

    Changing mapping giscus

--- src/app/content/[slug]/giscus_comp.tsx
+++ src/app/content/[slug]/giscus_comp.tsx
@@ -1,6 +1,6 @@
 'use client';
 import Giscus from "@giscus/react";
 export default function GiscusComp(){
-    return  <Giscus repoId="R_kgDOO3wdqg" categoryId="DIC_kwDOO3wdqs4CrnNr" repo="MuniMunii/muniblog" mapping="pathname" theme={"dark_tritanopia"} category="comments" loading="lazy"/>
+    return  <Giscus repoId="R_kgDOO3wdqg" categoryId="DIC_kwDOO3wdqs4CrnNr" repo="MuniMunii/muniblog" mapping="title" theme={"dark_tritanopia"} category="comments" loading="lazy"/>
 
 }
